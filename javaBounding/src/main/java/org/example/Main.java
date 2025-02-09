e19f4d739113741fd995936c5015a9d4a05bdf14
Matthew Peters <f1driver05@gmail.com>
2025-02-09 01:07:40 +0000

    fixed everything

--- javaBounding/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-// https://www.baeldung.com/opencsv
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
